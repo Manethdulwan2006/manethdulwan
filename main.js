62bfeb71462261fda087c3b60bdee8751a4620b9
Manethdulwan2006 <120876928+Manethdulwan2006@users.noreply.github.com>
2023-06-07 09:33:42 +0530

    update

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("Hellow Maneth Dulwan");
+    var email="manethdulwan@gmai.com"
+    console.log(email);
+    var studentname=90;
+     var mark=45.6;
+      var grade='A';
+      var x=12;
+      var y= 13;
+
+      var answer=x+y
+      console.log(answer);
+    function login() {
+        var num1=document.getElementById("num1").value
+        var num2=document.getElementById("num2").value
+      
+
+        var answer=(num1 + num2);
+        console.log(answer)
+
+
+    }
